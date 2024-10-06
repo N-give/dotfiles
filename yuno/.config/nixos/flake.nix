42a7c2ad756ed3adcd24a2857875c249d8fe6686
Nathan Givens <nate.givens03@gmail.com>
2024-10-06 00:50:28 -0400

    remove home-manager

--- yuno/.config/nixos/flake.nix
+++ yuno/.config/nixos/flake.nix
@@ -2,36 +2,13 @@
   description = "yuno system config";
 
   inputs = {
-    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
-    # nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, home-manager }:
-  let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {
-      inherit system;
-      config = { allowUnfree = true; };
-    };
-    lib = nixpkgs.lib;
-
-  in {
-    nixosConfigurations = {
-      yuno = lib.nixosSystem {
-        inherit system;
-
-        modules = [
-          ./configuration.nix
-          home-manager.nixosModules.home-manager {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.nate = import ./home.nix;
-          }
-        ];
-      };
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.yuno = nixpkgs.lib.nixosSystem rec {
+      system = "x86_64-linux";
+      modules = [ ./configuration.nix ];
     };
   };
 }
