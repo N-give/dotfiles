395e0dd0fdba2e49b28aea94cd0bb6132002d28f
Nathan Givens <nate.givens03@gmail.com>
2025-01-18 00:28:50 -0500

    add settings for plasma 6; not enabled

--- asta/.config/nixos/configuration.nix
+++ asta/.config/nixos/configuration.nix
@@ -108,6 +108,11 @@
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
+  # uncomment these and remove plasma5 to move to KDE Plasma6 DE
+  # services.xserver.enable = true;
+  # services.displayManager.sddm.enable = true;
+  # services.displayManager.sddm.wayland.enable = true;
+  # services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
