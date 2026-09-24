ff6f5b5c6e9013ee6c8bc528a28292a076909994
Nathan Givens <nate.givens03@gmail.com>
2026-09-24 13:47:41 -0400

    change string character

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-vim.opt.syntax = "on"
+vim.opt.syntax = 'on'
 vim.opt.showcmd = true
 vim.opt.laststatus = 2
 vim.opt.backspace = "indent,eol,start"
